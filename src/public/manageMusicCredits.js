f41019c39f5d266211343ffaba3e65c92bf8e303
Cody Romano <codyromano@gmail.com>
2020-04-15 22:16:22 -0400

    Create working Stripe sandbox session without auth

---
+++ src/public/manageMusicCredits.js
@@ -0,0 +1,37 @@
+// Replace this with your public key
+const stripe = Stripe("YOUR-PUBLIC-STRIPE-KEY");
+
+const checkout = async () => {
+  try {
+    const response = await fetch('/session', {
+      method: 'POST'
+    }).then(resp => resp.json());
+
+    if (response.error) {
+      throw new Error(response.error);
+    }
+
+    const result = await stripe.redirectToCheckout({
+      sessionId: response.id,
+    });
+
+    if (result.error) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const select = (selector) => document.querySelector(selector);
+const setText = (selector, text) => select(selector).textContent = text;
+
+document.addEventListener('click', async ({ target }) => {
+  if (target.matches('.buy-music-credits')) {
+    const checkoutOK = await checkout();
+    if (!checkoutOK) {
+      setText('purchase-status', 'Sorry, there was a problem purchasing music.');
+    }
+  }
+});
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,48 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const { PORT } = process.env;
+const {
+  PORT,
+  STRIPE_SECRET_KEY,
+  HTTP_URL,
+} = process.env;
 
-app.get('/', (req, res) => res.send('Hello, Money!'));
+const PUBLIC_PATH = path.join(__dirname, '../src/public');
+
+const stripe = require('stripe')(STRIPE_SECRET_KEY);
+
+app.post('/session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        name: 'Music credit',
+        description: 'Allows you to purchase songs',
+        images: ['https://example.com/t-shirt.png'],
+        amount: 500,
+        currency: 'usd',
+        quantity: 1,
+      }],
+      success_url: `${HTTP_URL}?checkout_complete&session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${HTTP_URL}?checkout_cancelled`,
+    });
+
+    return res.json({
+      id: session.id,
+      error: null,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.json({
+      error: 'There was problem starting your checkout session. Please try again.',
+      id: null,
+    });
+  }
+});
+
+app.use(express.static(PUBLIC_PATH));
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
